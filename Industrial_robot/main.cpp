3a803d98610a900ef00062221134b70a83dd230a
ddlBoJack <51037098+ddlBoJack@users.noreply.github.com>
2021-10-31 20:45:45 +0800

    Initial Commit

---
+++ Industrial_robot/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Industrial_robot
+//
+//  Created by BoJack on 2021/10/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
